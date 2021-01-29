67902e18e76c1b9886dbd1fc0743f231940814b5
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-01-29 17:17:47 +0100

    Add files via upload

---
+++ Домашна4/App_Navigacija_MVC2/Models/App_App_NavigacijaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace App_Navigacija_MVC2.Models
+{
+    public class App_App_NavigacijaContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
